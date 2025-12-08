a4f2b7fd267508aabbad27211e42c551a93b71f5
ElvexDom <n.bertrand@elvex-domotique.fr>
2025-12-08 20:33:57 +0100

    db_tools

--- backend/modules/db_tools.py
+++ backend/modules/db_tools.py
@@ -14,34 +14,34 @@ class Quote(Base):
     id = Column(Integer, primary_key=True)
     text = Column(String(100), nullable=False, unique=True)
 
-DB_FILE_PATH = "backend/data/quotes_db.db"
+DB_FILE_PATH = os.path.join("backend","data", "quotes_db.db")
 DATABASE_URL = f"sqlite:///{DB_FILE_PATH}"
 
 engine = create_engine(DATABASE_URL, echo=False)
 Session = sessionmaker(bind=engine)
 
 def write_db(df: pd.DataFrame):
-    session = Session()
-    for _, row in df.iterrows():
-        # Crée un objet Quote si texte unique
-        if not session.query(Quote).filter_by(text=row['text']).first():
-            session.add(Quote(id=row['id'], text=row['text']))
-    session.commit()
-    session.close()
+    with Session() as session:
+        row = df.iloc[-1]
+        exists = session.query(Quote).filter_by(text=row['text']).first()
+        if not exists:
+            session.add(Quote(text=row['text']))
+        session.commit()
+
 
 def read_db() -> pd.DataFrame:
-    session = Session()
-    quotes = session.query(Quote).all()
-    session.close()
-    # Convertir en DataFrame
+    with Session() as session:
+        quotes = session.query(Quote).all()
     df = pd.DataFrame([{"id": q.id, "text": q.text} for q in quotes])
-    df.set_index('id', inplace=True)
+    if not df.empty:
+        df.set_index("id", inplace=True)
+        print(f"ma valeur est la suivante {df}")
     return df
 
 def initialize_db():
-    if os.path.exists(DATABASE_URL):
+    if os.path.exists(DB_FILE_PATH):
         logger.info("La base de données existe")
     else:
-        logger.info(f"impossible de trouver le fichier {DATABASE_URL}")
+        logger.info(f"impossible de trouver le fichier {DB_FILE_PATH}")
         Base.metadata.create_all(engine)
-        logger.info(f"le fichier {DATABASE_URL} a été créé")
+        logger.info(f"le fichier {DB_FILE_PATH} a été créé")
